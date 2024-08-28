9cf897f307eac0179fee941b728113911c0258b8
Ed Bueler <elbueler@alaska.edu>
2024-08-28 15:57:14 -0800

    allow other codes to use _PinchColumnXXX objects

--- stokesextrude/__init__.py
+++ stokesextrude/__init__.py
@@ -6,9 +6,16 @@ __all__ = [
     "extend_p1_from_basemesh",
     "trace_scalar_to_p1",
     "trace_vector_to_p2",
+    "_PinchColumnVelocity",
+    "_PinchColumnPressure",
 ]
 
-from .stokesextrude import StokesExtrude, printpar
+from .stokesextrude import (
+    StokesExtrude,
+    printpar,
+    _PinchColumnVelocity,
+    _PinchColumnPressure,
+)
 from .solverparams import SolverParams, pc_Mass
 from .traceextend import (
     extend_p1_from_basemesh,
